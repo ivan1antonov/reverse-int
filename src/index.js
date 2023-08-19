d3527ddba7b577957e8b113611ab32b7a6c2894b
ivan1antonov <ivan1antonov@gmail.com>
2023-08-19 20:40:18 +0400

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+  let temp = String(Math.abs(n));
+  let res = '';
+  for (let i = temp.length-1; i >= 0; i--){
+    res = res + temp[i];
+  }
+  return res;
+};
